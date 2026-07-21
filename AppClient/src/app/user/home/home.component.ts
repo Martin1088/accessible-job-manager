116d2ad0965f809c639e67c54ea48519196a6207
Martin1088 <jurkmartin88@gmail.com>
2026-07-21 13:51:39 +0200

    Translate the user home page (EN/DE/NL)
    This page was never wired into ngx-translate - title, job-import card,
    profile-error alert, and quick-nav links were all hardcoded English.
    Also fixes home.component.spec.ts's now-missing TranslateService
    provider (same class of fix as the other specs).

--- AppClient/src/app/user/home/home.component.ts
+++ AppClient/src/app/user/home/home.component.ts
@@ -3,11 +3,12 @@ import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 import { AuthService, UserMe } from '../../core/auth.service';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, CommonModule, FormsModule],
+  imports: [RouterLink, CommonModule, FormsModule, TranslatePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
